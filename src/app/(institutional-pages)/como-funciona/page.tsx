65dce295d8c52f8b92fb654ff149c44da1d587b3
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-19 09:38:59 -0300

    feat(pages como-funciona): create page skeleton

---
+++ src/app/(institutional-pages)/como-funciona/page.tsx
@@ -0,0 +1,39 @@
+import { NextPage } from 'next'
+
+import { ClientsCarousel } from '@/components/common/ClientsCarousel'
+import { Footer } from '@/components/common/Footer'
+import { Navbar } from '@/components/common/Navbar'
+import { getMetaData } from '@/utils/getters/getMetaData'
+
+import { HowItWorks } from '../entregas/components/HowItWorks'
+import { Stats } from '../entregas/components/Stats'
+import { Header } from './components/Header'
+import { Procedures } from './components/Procedures.tsx'
+import { Productivity } from './components/Productivity'
+
+export async function generateMetadata() {
+  return getMetaData({
+    title: '',
+    description: '',
+    opengraph: ''
+  })
+}
+
+const Page: NextPage = async () => {
+  return (
+    <>
+      <Navbar />
+      <main className="pt-10 lg:pt-[44px]">
+        <Header />
+        <ClientsCarousel />
+        <Productivity />
+        <Procedures />
+        <Stats />
+        <HowItWorks />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Page
