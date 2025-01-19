e0ce3f87e238e9bb86d6174d0c2758c53c7e308b
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-19 09:39:59 -0300

    feat(pages como-funciona): added procedures section

---
+++ src/app/(institutional-pages)/como-funciona/components/Procedures/data.ts
@@ -0,0 +1,17 @@
+export const PROCEDURES_SECTIONS = [
+  {
+    title: 'Escolha e compre facilmente',
+    description:
+      'Encontre os produtos de que precisa em mercados parceiros próximos a você, organizados por categorias para facilitar a navegação. Adicione itens ao carrinho com apenas alguns cliques e escolha a quantidade desejada sem complicações.'
+  },
+  {
+    title: 'Pague de forma segura e prática',
+    description:
+      'Realize o pagamento com métodos que se adaptam às suas preferências, como cartões de crédito, débito ou carteiras digitais. Garantimos uma transação segura com tecnologia de ponta para proteger seus dados financeiros.'
+  },
+  {
+    title: 'Receba sua entrega rapidamente',
+    description:
+      'Após a confirmação do pagamento, nossos entregadores iniciam o processo de transporte imediato. Receba seus produtos frescos e de qualidade no endereço escolhido em tempo recorde.'
+  }
+]
---
+++ src/app/(institutional-pages)/como-funciona/components/Procedures/index.tsx
@@ -0,0 +1,31 @@
+import { PROCEDURES_SECTIONS } from './data'
+
+export const Procedures: React.FC = async () => {
+  return (
+    <section className="flex flex-col gap-8 bg-white px-4 py-12 lg:gap-12 lg:py-20">
+      {PROCEDURES_SECTIONS.map((section, index: number) => (
+        <div
+          className="mx-auto flex w-full max-w-2xl flex-col-reverse gap-8 border-b border-neutral-300 pb-8 last:border-transparent lg:max-w-7xl lg:flex-row lg:items-center lg:justify-between lg:gap-16 lg:pb-12"
+          key={`${section.title}-${index}`}
+        >
+          <article
+            className={`flex w-full flex-col gap-4 lg:gap-6 ${index % 2 === 0 ? 'lg:order-1' : 'lg:order-2'}`}
+          >
+            <h2 className="text-2xl font-medium lg:text-3xl">
+              {section.title}
+            </h2>
+            <div className="flex flex-col gap-4">
+              <p
+                className="text-sm text-neutral-600 lg:text-base"
+                dangerouslySetInnerHTML={{ __html: section.description }}
+              />
+            </div>
+          </article>
+          <figure
+            className={`min-h-[320px] w-full rounded-sm bg-slate-200 ${index % 2 === 0 ? 'lg:order-2' : 'lg:order-1'}`}
+          />
+        </div>
+      ))}
+    </section>
+  )
+}
