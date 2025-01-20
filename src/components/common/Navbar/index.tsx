cfa152a8ab5ee52ff204640ee485284d15e7d4d6
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-19 21:50:41 -0300

    fix(components navbar) adjusted z index value

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -6,7 +6,7 @@ import { Shop } from './icons/Shop'
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="fixed inset-0 max-h-[44px] w-full bg-[#f5f5f7c2] bg-opacity-10 bg-clip-padding px-6 py-4 backdrop-blur-sm backdrop-filter lg:px-4 xl:px-0">
+    <nav className="fixed inset-0 z-[99999] max-h-[44px] w-full bg-[#f5f5f7c2] bg-opacity-10 bg-clip-padding px-6 py-4 backdrop-blur-sm backdrop-filter lg:px-4 xl:px-0">
       <div className="mx-auto flex w-full max-w-7xl items-center justify-center gap-4 lg:gap-8">
         {NAVBAR_LINKS.map((navbarLink, index) => (
           <Anchor
