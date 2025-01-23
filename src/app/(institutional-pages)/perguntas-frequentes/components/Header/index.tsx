9ecd596b26be7d245ff4135c7dbc7e4ed866178a
vitingr <vitorgabrielsbo1460@gmail.com>
2025-01-22 21:12:06 -0300

    fix(pages perguntas-frequentes): adjusted containers

--- src/app/(institutional-pages)/perguntas-frequentes/components/Header/index.tsx
+++ src/app/(institutional-pages)/perguntas-frequentes/components/Header/index.tsx
@@ -4,8 +4,9 @@ export const Header: React.FC = async () => {
   return (
     <Container
       as="header"
+      className="mx-auto flex w-full flex-col gap-2"
       data-cid="faq-header"
-      wrapperClassName="mx-auto flex w-full max-w-2xl flex-col gap-2 lg:max-w-7xl"
+      wrapperClassName="mx-auto w-full max-w-2xl lg:max-w-7xl"
       disableSidePadding
     >
       <p className="animate__animated animate__fadeIn bg-gradient-to-r from-amber-600 to-amber-800 bg-clip-text text-center text-sm font-semibold text-transparent lg:text-left lg:text-base">
--- src/app/(institutional-pages)/perguntas-frequentes/components/Questions/index.tsx
+++ src/app/(institutional-pages)/perguntas-frequentes/components/Questions/index.tsx
@@ -7,8 +7,9 @@ export const Questions: React.FC = async () => {
   return (
     <Container
       as="section"
+      className="flex w-full flex-col gap-4 lg:gap-6"
       data-cid="faq-questions"
-      wrapperClassName="mx-auto flex w-full max-w-2xl flex-col gap-4 lg:max-w-7xl lg:gap-6"
+      wrapperClassName="mx-auto w-full max-w-2xl  lg:max-w-7xl"
       disableSidePadding
     >
       {FAQ_QUESTIONS.map((question, index: number) => (
