f746fe5d0a9653e55e2cc795c3a67e57b1ed167c
LuisbarbosaGit <luisbarbosa.santos@outlook.com>
2025-04-29 13:01:41 -0300

    Começando o cabeçalho da lp acelerando seu negocio

--- src/app/(landing-pages)/acelerando-seu-negocio/page.tsx
+++ src/app/(landing-pages)/acelerando-seu-negocio/page.tsx
@@ -1,8 +1,9 @@
 import { NextPage } from 'next'
-
 import { Footer } from '@/components/common/Footer'
 import { Navbar } from '@/components/common/Navbar'
 import { getMetaData } from '@/utils/seo/getMetaData'
+import { ClientsCarousel } from '@/components/common/ClientsCarousel'
+
 
 export async function generateMetadata() {
   return getMetaData({
@@ -16,7 +17,14 @@ const Page: NextPage = async () => {
   return (
     <>
       <Navbar />
-      <main>/acelerando-seu-negocio</main>
+      <main className="w-screen flex pt-20 space-y-N items-center p-2 m-2 h-32 justify-center uppercase">
+        <h1 className="text-5xl font-bold text-gray-600 text-blue-900">Acelerando seu negócio</h1>
+      </main>
+      <p className='text-center text-2xl'>Soluções avançadas para a sua empresa</p>
+      <ClientsCarousel/>
+      <div>
+        
+      </div>
       <Footer />
     </>
   )
